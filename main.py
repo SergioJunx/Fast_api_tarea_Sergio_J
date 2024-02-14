6819b3c4b168bcb0fc0c355440f644e39d52f445
Sergio <sjm0005@alu.medac.es>
2024-02-14 19:35:15 +0100

    Update

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/calcular-longitud/{cadena}")
+def read_item(cadena: str):
+    contador = 0
+    for letra in cadena:
+        if letra in "aieou":
+            contador += 1
+    longitud_cad = len(cadena)
+    return {"La cadena introducida es: ": cadena, "La longitud de la palabra introducida es: ": longitud_cad, "Numero de letras: ": len(cadena)}
+
